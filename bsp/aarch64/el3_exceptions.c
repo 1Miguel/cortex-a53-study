11ad537f3014b727dc6ec5de57ef89084471f960
jugsimar <jmjugsimar@gmail.com>
2026-09-11 00:36:42 +0800

    checkpoint update

--- bsp/aarch64/el3_exceptions.c
+++ bsp/aarch64/el3_exceptions.c
@@ -1,1 +1,39 @@
+void __attribute__((weak))el3_sp0_sync_handler(void)
+{
+  while(1);
+}
 
+void __attribute__((weak))el3_sp0_irq_handler(void)
+{
+  while(1);
+}
+
+void __attribute__((weak))el3_sp0_fiq_handler(void)
+{
+  while(1);
+}
+
+void __attribute__((weak))el3_sp0_serror_handler(void)
+{
+  while(1);
+}
+
+void __attribute__((weak))el3_spx_sync_handler(void)
+{
+  while(1);
+}
+
+void __attribute__((weak))el3_spx_irq_handler(void)
+{
+  while(1);
+}
+
+void __attribute__((weak))el3_spx_fiq_handler(void)
+{
+  while(1);
+}
+
+void __attribute__((weak))el3_spx_serror_handler(void)
+{
+  while(1);
+}
